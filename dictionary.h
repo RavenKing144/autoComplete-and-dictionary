0c5b3b8964a9f68105a126e6a238a744089ba6ac
RavenKing144 <agarwalsarthak129@gmail.com>
2018-11-02 22:24:35 +0530

    dictionary.h working properly

--- dictionary.h
+++ dictionary.h
@@ -1,112 +1,136 @@
 
-struct avl_node
+struct Node
 {
-  string word;
-  string meaning;
-  avl_node * right;
-  avl_node * left;
-  int height;
+    string word;
+    string mean;
+    Node *left;
+    Node *right;
+    int height;
 };
+
+
 int max(int a, int b);
 
-int height(avl_node *N)
+
+int height(Node *N)
 {
     if (N == NULL)
         return 0;
     return N->height;
 }
 
-int max(int a,int b)
+int max(int a, int b)
 {
     return (a > b)? a : b;
 }
 
-avl_node * newNode(string word,string meaning)
+
+struct Node* newNode(string word, string mean)
 {
-  avl_node * node= new avl_node;
-  node->word=word;
-  node->meaning=meaning;
-  node->left=NULL;
-  node->right=NULL;
-  node->height=1;
-  return node;
+    Node* node = new Node;
+    node->word = word;
+    node->mean = mean;
+    node->left   = NULL;
+    node->right  = NULL;
+    node->height = 1;  
+    return(node);
 }
 
-avl_node * rightRotate(avl_node * y)
+Node *rightRotate(Node *y)
 {
-  avl_node * x=y->left;
-  avl_node * temp=x->right;
-  x->right=y;
-  y->left=temp;
-  y->height=max(height(y->left),height(y->right))+1;
-  x->height=max(height(x->left),height(x->right))+1;
-  return x;
+    Node *x = y->left;
+    Node *T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    y->height = max(height(y->left), height(y->right))+1;
+    x->height = max(height(x->left), height(x->right))+1;
+
+    return x;
 }
 
-avl_node * leftRotate(avl_node * x)
+Node *leftRotate(Node *x)
 {
-  avl_node * y=x->right;
-  avl_node * temp=y->left;
-  y->left=x;
-  x->right=temp;
-  y->height=max(height(y->left),height(y->right))+1;
-  x->height=max(height(x->left),height(x->right))+1;
-  return y;
+    Node *y = x->right;
+    Node *T2 = y->left;
+    y->left = x;
+    x->right = T2;
+
+    x->height = max(height(x->left), height(x->right))+1;
+    y->height = max(height(y->left), height(y->right))+1;
+
+    
+    return y;
 }
 
-int Balance(avl_node * n)
+
+int getBalance(Node *N)
 {
-  if(n==NULL)
-    return 0;
-  return height(n->left)-height(n->right);
+    if (N == NULL)
+        return 0;
+    return height(N->left) - height(N->right);
 }
 
-avl_node * insert(avl_node * node,string word,string meaning)
+
+Node* insert(Node* node, string word, string mean )
 {
-  if(node==NULL)
-    return(newNode(word,meaning));;
+    
+    if (node == NULL)
+        return(newNode(word,mean));
 
-  if(word<node->word)
-    node->left=insert(node->left,word,meaning);
-  else if(word>node->word)
-    node->right=insert(node->right,word,meaning);
-  else
-    return node;
-  node->height=1+max(height(node->left), height(node->right));
-  int balance =Balance(node);
+    if (word < node->word)
+        node->left  = insert(node->left, word,mean);
+    else if (word > node->word)
+        node->right = insert(node->right, word,mean);
+    else 
+        return node;
 
-  if (balance > 1 && word < node->left->word)
+    
+    node->height = 1 + max(height(node->left),
+                           height(node->right));
+
+    
+    int balance = getBalance(node);
+
+   
+    if (balance > 1 && word < node->left->word)
         return rightRotate(node);
 
     if (balance < -1 && word > node->right->word)
         return leftRotate(node);
 
-    if (balance > 1 && word> node->left->word)
+    
+    if (balance > 1 && word > node->left->word)
     {
         node->left =  leftRotate(node->left);
         return rightRotate(node);
     }
 
-    if (balance < -1 && word< node->right->word)
+    
+    if (balance < -1 && word < node->right->word)
     {
         node->right = rightRotate(node->right);
         return leftRotate(node);
     }
 
-return node;
-
+    
+    return node;
 }
- avl_node * search(avl_node * root,string word)
- {
-  if(root!=NULL)
-  {
-    if(root->word==word)
-      return root;
-    if(root->word<word)
-      return search(root->right,word);
-    if(root->word>word)
-      return search(root->left,word);
-  }
-  cout<<"not found"<<endl;
-  }
+
+
+Node* search(Node* root, string word)
+{
+   
+    if (root == NULL || root->word == word)
+       return root;
+
+    
+    if (root->word < word)
+       return search(root->right, word);
+
+    
+    return search(root->left, word);
+}
+
 
